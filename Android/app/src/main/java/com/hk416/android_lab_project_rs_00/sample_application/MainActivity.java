e802c62f252fd7e4682af57b43d112c40b9bba7e
HK416 <powerspirit127@gmail.com>
2024-03-11 11:55:51 +0900

    Initialize Android project.

---
+++ Android/app/src/main/java/com/hk416/android_lab_project_rs_00/sample_application/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hk416.android_lab_project_rs_00.sample_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
